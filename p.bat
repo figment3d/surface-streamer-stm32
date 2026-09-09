20ea3d7d67db78a6897f54132e1da7f5ff2f2237
figment3d <neil.mclaughlin@figment3d.com>
2026-09-09 13:14:47 -0400

    Configure FDCAN and add CAN adapter health check

--- p.bat
+++ p.bat
@@ -2,4 +2,5 @@
 call p1.bat
 call p2.bat
 call p3.bat
+call p4.bat
 
---
+++ p4.bat
@@ -0,0 +1,2 @@
+@echo off
+python -c "import can; b=None; exec(\"try:\n b=can.Bus(interface='gs_usb', channel=0, bitrate=500000)\n print('CAN_ADAPTER_READY gs_usb')\nexcept Exception:\n print('CAN_ADAPTER_OFFLINE')\nfinally:\n b.shutdown() if b else None\")"
